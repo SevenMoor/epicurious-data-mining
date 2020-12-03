b3b535c52cc976012522ac4296a7bf3249b0e341
SevenMoor <au.ottaviano@yahoo.com>
2020-12-03 15:54:02 +0100

    Add: Dataframe

--- script.R
+++ script.R
@@ -1,4 +1,7 @@
 rm(list=ls())
 library(data.table)
 
-df <- csv.read(file='datas/',sep=";",col_c)
+df <- read.csv(file='datas/RAW_recipes.csv',sep=",",colClasses = c(NA,NA,"NULL","NULL","NULL","NULL",NA,"NULL","NULL","NULL",NA,"NULL"))
+reviews <- read.csv(file ='datas/RAW_interactions.csv',sep=",",colClasses = c("NULL",NA,"NULL",NA,"NULL"))
+reviews <- aggregate(reviews[2],reviews[1],mean)
+recipes = merge(df,reviews,by.x = "id",by.y = "recipe_id")[2:length(recipes)]
